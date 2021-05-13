19d2f4ec6529d025e98adaa9b061364e081139dc
Aman <amanmalviya14032000@gmail.com>
2021-05-13 17:24:22 +0530

    Frontend : Designed Login and register window

--- Client/src/Components/App.js
+++ Client/src/Components/App.js
@@ -1,25 +1,34 @@
 import {Component} from 'react';
+import Login from './Login'
+import Register from './Register'
 
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = { apiResponse: "" };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { apiResponse: "" };
+  // }
 
-  callAPI() {
-      fetch("http://localhost:9000/testAPI")
-          .then(res => res.text())
-          .then(res => this.setState({ apiResponse: res }));
-  }
+  // callAPI() {
+  //     fetch("http://localhost:9000/testAPI")
+  //         .then(res => res.text())
+  //         .then(res => this.setState({ apiResponse: res }));
+  // }
 
-  componentWillMount() {
-      this.callAPI();
-  }
+  // componentWillMount() {
+  //     this.callAPI();
+  // }
+
   render(){
     return (
       <div className="App">
-        <p className="App-intro">{this.state.apiResponse}</p>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Login} />
+            <Route path="/register" component={Register} />
+          </Switch>
+        </Router>
       </div>
     );
   }
---
+++ Client/src/Components/PasswordStrength.js
@@ -0,0 +1,42 @@
+import {passwordStrength as PST} from 'check-password-strength'
+import {useEffect, useState} from 'react'
+export default function PassSt({pwd}){
+    const [b1, setb1]=useState('rgba(0,0,0,0.1)');
+    const [b2, setb2]=useState('rgba(0,0,0,0.1)');
+    const [b3, setb3]=useState('rgba(0,0,0,0.1)');
+    const [flag, setFlag]=useState('');
+    const [vis, setVis]=useState('hidden');
+
+    useEffect(()=>{
+        let status=PST(pwd).value;
+        if(pwd === '')
+            setVis('hidden');
+        else{
+            setVis('visible');
+            if(status === "Too weak"){
+                setb1('#df1623');
+                setb2('rgba(0,0,0,0.1)');
+                setb3('rgba(0,0,0,0.1)');
+                setFlag('Weak');
+            }else if(status === "Weak"){
+                setb1('#d5b134');
+                setb2('#d5b134');
+                setb3('rgba(0,0,0,0.1)');
+                setFlag('Medium');
+            }else{
+                setb1("#70e000");
+                setb2('#70e000');
+                setb3('#70e000');
+                setFlag("Strong");
+            }
+        }
+    }, [pwd])
+    return (
+        <div style={{'visibility':vis}} className='d-inline-block align-items-center'>
+            <div id='b-1' style={{'backgroundColor':b1, 'height':'5px', 'width':'50px', 'marginRight':'5px'}} className="d-inline-block shadow-sm rounded-pill"></div>
+            <div id='b-2' style={{'backgroundColor':b2, 'height':'5px', 'width':'50px', 'marginRight':'5px'}} className="d-inline-block shadow-sm rounded-pill"></div>
+            <div id='b-3' style={{'backgroundColor':b3, 'height':'5px', 'width':'50px', 'marginRight':'5px'}} className="d-inline-block shadow-sm rounded-pill"></div>
+            <p className="grey small">{flag}</p>
+        </div>
+    )
+}
